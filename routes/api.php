ab68ac58cf276e0d26eca5e94eb3518a4e15b42a
claraanjelitakd <claraanjelitakd@gmail.com>
2025-11-30 09:29:18 +0700

    Manajemen Bahasa dan Manajemen Paket

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\UserController as ControllersUserController;
+use App\Http\Controllers\BahasaController;
+use App\Http\Controllers\PaketController;
 
 // Cek API
 Route::get('/check', function () {
@@ -24,9 +27,8 @@ Route::get('/kursus', [AuthController::class, 'kursus']);
 
 // Versi API
 Route::prefix('/admin')->group(function () {
-
+    //user
     Route::get('users', [UserController::class, 'index']);
-
     Route::get('/status', function () {
         return response()->json(['version' => 'v1', 'status' => 'ok']);
     });
@@ -34,6 +36,19 @@ Route::prefix('/admin')->group(function () {
     Route::delete('users/{id}', [UserController::class, 'destroy']);
     Route::patch('users/{id}/toggle-verify', [UserController::class, 'toggleVerify']);
     Route::post('users', [UserController::class, 'store']);
+    // === PAKET CRUD ===
+    Route::get('paket', [PaketController::class, 'index']);
+    Route::get('paket/{id}', [PaketController::class, 'show']);
+    Route::post('paket', [PaketController::class, 'store']);
+    Route::put('paket/{id}', [PaketController::class, 'update']);
+    Route::delete('paket/{id}', [PaketController::class, 'destroy']);
+
+    // === BAHASA CRUD ===
+    Route::get('bahasa', [BahasaController::class, 'index']);
+    Route::get('bahasa/{id}', [BahasaController::class, 'show']);
+    Route::post('bahasa', [BahasaController::class, 'store']);
+    Route::put('bahasa/{id}', [BahasaController::class, 'update']);
+    Route::delete('bahasa/{id}', [BahasaController::class, 'destroy']);
 
     // Protected routes (harus login pakai Sanctum)
     Route::middleware('auth:sanctum')->group(function () {
